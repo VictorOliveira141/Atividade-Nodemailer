d63934a0f0d1c3f47f5a1d57b3ddb4cf95f91407
VictorOliveira141 <rm94501@estudante.fieb.edu.br>
2025-11-09 14:32:53 +0000

    up3

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.set("view engine", "ejs");
 app.set("views", "./app/views");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-var rotas = require("./app/routes/router")
-app.use("/", rotas)
+var rotas = require("./app/routes/router");
+app.use("/", rotas);
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Servidor online 🔥\nhttp://localhost:${process.env.APP_PORT}`);
--- app/routes/router.js
+++ app/routes/router.js
@@ -1,8 +1,45 @@
 var express = require("express");
 var router = express.Router();
+var nodemailer = require("nodemailer");
 
 router.get("/", (req, res) => {
-    res.render("pages/dotenv");
+  res.render("pages/dotenv");
+});
+
+router.post("/dotenv", (req, res) => {
+  // NODEMAILER
+  const { nome, mensagem } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
   });
 
-  module.exports = router;
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: "Formulário enviado com Nodemailer",
+    html: `
+        <h2>Novo envio do formulário</h2>
+        <p><b>Nome:</b> ${nome}</p>
+        <p><b>Mensagem:</b> ${mensagem}</p>
+      `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Erro:", error);
+      return res.send("Erro ao enviar e-mail");
+    }
+
+    console.log("Email enviado:", info.response);
+    res.send("E-mail enviado com sucesso!");
+  });
+});
+
+module.exports = router;
